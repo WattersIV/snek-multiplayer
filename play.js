02d01efda287070c0c70c3105f4cc8a6a4242804
Bill Watters <billywatters13@gmail.com>
2020-09-23 16:59:27 +0000

    Added server messages to be console logged to player

--- play.js
+++ play.js
@@ -9,3 +9,21 @@ const game = new Game(new UserInterface(), new RemoteInterface())
 
 // Begin game
 game.start()
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8');
+  conn.on('data', (message) => console.log(message))
+  return conn;
+}
+
+console.log('Connecting ...');
+connect();
